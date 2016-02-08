949054ad728a27780aec9b6fa85b78d5a65621fa
Benjamin Appiah-Brobbey <Benjamin Appiah-Brobbey>
2016-02-08 18:49:35 +0000

    added settings

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'unilynq');
+define('DB_NAME', 'unilynqserver');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'adminXxiNiBV');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'xaZJGltL6chc');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
